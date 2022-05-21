6b17d8069023fbdab456580c0bd5b95cef99784e
concordance <asterartworks@gmail.com>
2022-05-21 22:10:20 +0200

    styles

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,9 +3,8 @@ export const Home = () => {
     return(
         <div className="container">
             <h1>Liquify your Optipunk.</h1>
-            <p>Make your optipunk divisible and get acess to liquidity.
+            <p className="main-home">Make your optipunk divisible and get acess to liquidity.
                 LiquidPunk turns your NFT into 100 ERC20 Tokens. 
-            
             </p>
             
         </div>
