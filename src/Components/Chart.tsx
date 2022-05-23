81dbf8f33338e18f695c78f63f9ba29542fdcc92
concordance <dietachmair.marcus@gmail.com>
2022-05-23 15:00:36 +0200

    UI fixes

---
+++ src/Components/Chart.tsx
@@ -0,0 +1,12 @@
+import { AdvancedChart } from "react-tradingview-embed";
+
+export const Chart = () => {
+    
+    return(
+        <>
+        <AdvancedChart widgetProps={{"theme": "light"}}/>
+        </>
+    )
+
+}
+
--- src/Pages/Trade.tsx
+++ src/Pages/Trade.tsx
@@ -1,10 +1,25 @@
+import {Chart} from '../Components/Chart'
+
 
 export const Trade = () => {
 
     return(
         <div className="container">
             <h1>Trade</h1>
-            <p>Order book exchange for trading the oPUNK/ETH. Coming Soon</p>
+            <p>Order book exchange for trading oPUNK/ETH. Coming Soon</p>
+            <div className='frame' hidden>
+                <div className='row'>
+                    <div className='col-8'>
+                        <Chart />
+
+                    </div>
+                    <div className='col-4'>
+                        <p>Test</p>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     );
 }
